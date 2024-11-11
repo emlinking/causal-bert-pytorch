5c81c73a272d06b5367494e1011b6155f73058dd
Eleanor Lin <emlinking@gmail.com>
2024-11-11 16:50:51 -0500

    fix debug in CausalBert

--- CausalBert.py
+++ CausalBert.py
@@ -222,9 +222,13 @@ class CausalBertWrapper:
             self.model.train()
 
             for step, batch in tqdm(enumerate(dataloader), total=len(dataloader)):
-                for x in batch:
-                    assert not np.any(np.isnan(x))
-                    
+                assert not np.any(np.isnan(batch[0]))
+                assert not np.any(np.isnan(batch[1]))
+                assert not np.any(np.isnan(batch[2]))
+                assert not np.any(np.isnan(batch[3]))
+                assert not np.any(np.isnan(batch[4]))
+                assert not np.any(np.isnan(batch[5]))
+
                 if CUDA: 
                     batch = (x.cuda() for x in batch)
                 W_ids, W_len, W_mask, C, T, Y = batch
